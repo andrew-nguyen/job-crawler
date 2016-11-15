b2cbaec58129836e90b727dc4cc68bde13ad8dfc
Andrew Nguyen <andrew.nguyen@usfca.edu>
2016-11-15 10:26:07 -0600

    Upgrade deps

--- project.clj
+++ project.clj
@@ -6,16 +6,16 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
 
-  :dependencies [[org.clojure/clojure "1.6.0"]
+  :dependencies [[org.clojure/clojure "1.9.0-alpha14"]
 
                  ; http client
-                 [clj-http "1.0.1"]
+                 [clj-http "3.3.0"]
 
                  ; for benchmarking code
-                 [criterium "0.4.3"]
+                 [criterium "0.4.4"]
 
                  ; scraping library
-                 [enlive "1.1.5"]]
+                 [enlive "1.1.6"]]
 
   :repl-options {:init-ns job-crawler.core}
 
